98aff7e3510445e9658caa78366ad235463aeebb
Maxime Mangel <mangel.maxime@protonmail.com>
2024-03-30 16:25:45 +0100

    chore: add test for typeAlias with a direct typeRefrerence
    [converter]

---
+++ tests/specs/references/typeAlias/typeReference.d.ts
@@ -0,0 +1,1 @@
+type StringArray = Array<string>
---
+++ tests/specs/references/typeAlias/typeReference.fsx
@@ -0,0 +1,12 @@
+module rec Glutinum
+
+open Fable.Core
+open Fable.Core.JsInterop
+open System
+
+type StringArray =
+    ResizeArray<string>
+
+(***)
+#r "nuget: Fable.Core"
+(***)
