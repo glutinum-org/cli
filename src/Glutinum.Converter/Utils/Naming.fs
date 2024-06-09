ee4ee995773249067c330ba4eaf35377e90017fc
Maxime Mangel <mangel.maxime@protonmail.com>
2024-06-09 16:40:26 +0200

    chore: remove commented code from Naming.fs

--- src/Glutinum.Converter/Utils/Naming.fs
+++ src/Glutinum.Converter/Utils/Naming.fs
@@ -2,23 +2,6 @@ module Naming
 
 open System
 
-/// <summary>
-/// Check if the value is the same as the default Fable computed value computed from the name.
-/// </summary>
-/// <param name="name">Name to be transformed by Fable</param>
-/// <param name="value">Value to compare to</param>
-/// <returns>
-/// <c>true</c> if provided value is the same as the default Fable computed value computed from the name.
-/// <c>false</c> otherwise
-/// </returns>
-// let nameEqualsDefaultFableValue (name: string) (value: string) : bool =
-//     let defaultFableValue = String.lowercaseFirstLetter name
-
-//     defaultFableValue.Equals value
-
-// let nameNotEqualsDefaultFableValue (name: string) (value: string) : bool =
-//     not (nameEqualsDefaultFableValue name value)
-
 let private startWithDigit (name: string) : bool =
     name.Length > 0 && Char.IsDigit name.[0]
 
