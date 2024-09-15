4d4a07fc6a98fd3a953ce60ceb5f970483bd5ce4
Maxime Mangel <mangel.maxime@protonmail.com>
2024-09-15 16:51:50 +0200

    chore: fix changelog generation

--- src/Glutinum.Build/Commands/Publish.fs
+++ src/Glutinum.Build/Commands/Publish.fs
@@ -278,8 +278,13 @@ let private updateChangelog (releaseContext: ReleaseContext) =
                 tryFindAdditionalChangelogContent commit.OriginalCommit.Message
                 // Indent the additional lines to be under item bullet point
                 |> List.map (fun line -> $"    %s{line}")
-                // Trim the lines (this is mostly to spaces on "empty" lines)
-                |> List.map _.Trim()
+                // Trim empty lines
+                |> List.map (fun line ->
+                    if String.IsNullOrWhiteSpace line then
+                        ""
+                    else
+                        line
+                )
 
             if not additionalChangelogContent.IsEmpty then
                 appendLine ""
