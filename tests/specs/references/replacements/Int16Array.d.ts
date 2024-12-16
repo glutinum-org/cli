01fa7828d9058ca23cec7d19691163926430d910
Nat Elkins <nathaniel.elkins@gmail.com>
2024-12-16 05:03:41 -0500

    feat: add support for additional Fable.Core.JS types (#172)
    [converter]
    
    === changelog ===
    - `Int8Array`
    - `Uint8ClampedArray`
    - `Int16Array`
    - `Uint16Array`
    - `Int32Array`
    - `Uint32Array`
    - `Float32Array`
    - `Float64Array`
    === changelog ===

---
+++ tests/specs/references/replacements/Int16Array.d.ts
@@ -0,0 +1,1 @@
+export type T = Int16Array;
---
+++ tests/specs/references/replacements/Uint16Array.fsx
@@ -0,0 +1,13 @@
+module rec Glutinum
+
+open Fable.Core
+open Fable.Core.JsInterop
+open System
+
+type T =
+    JS.Uint16Array
+
+(***)
+#r "nuget: Fable.Core"
+#r "nuget: Glutinum.Types"
+(***)
